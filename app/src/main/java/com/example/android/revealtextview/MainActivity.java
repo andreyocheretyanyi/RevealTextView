c9110c6db9d75babcdbc345c122a03727ff8fc6d
andrey.ocheretyanyi <andrey.ocheretyanyi@gmail.com>
2017-11-26 19:54:10 +0300

    created anim

--- app/src/main/java/com/example/android/revealtextview/MainActivity.java
+++ app/src/main/java/com/example/android/revealtextview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.revealtextview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RevealTextView textView = findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((RevealTextView) v).startAnim();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/revealtextview/RevealTextView.java
+++ app/src/main/java/com/example/android/revealtextview/RevealTextView.java
@@ -4,27 +4,33 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.Paint;
-import android.graphics.PixelFormat;
+import android.graphics.Path;
 import android.graphics.RectF;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.view.View;
 
 /**
  * Created by Android on 23.11.2017.
  */
 
-public class RevealTextView extends AppCompatTextView {
+public class RevealTextView extends View {
 
-    TextDrawable first, second;
-    RectF boundsFirst, boundsSecond;
-    float step = 0.1f;
-    private boolean isFirstNeedRedraw = true, isSecondNeedRedraw = true;
-    private boolean isFirstLaunch = true;
+
+    public static final int mFps = 60;
+
+    private int animationTime = 2000;
+    private int mCurrentInvalidateCount;
+    private int mMaxInvalidateCount;
+    private Path mPath;
+    private float mChangedRadius;
+    private boolean isFirstLayerVisible = false;
+    private int mHighAlpha = 255;
+    private int mLowAlpha = 0;
+    private int mFirstTextColor = Color.RED, mSecondTextColor = Color.BLACK, mFirstBackColorColor = Color.BLACK, mSecondBackColorColor = Color.RED;
+    private int mTextSize = 20;
 
     public RevealTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -32,42 +38,97 @@ public class RevealTextView extends AppCompatTextView {
 
     public RevealTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+    }
+
+    private void initAttrs(AttributeSet attrs) {
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (isFirstLaunch) {
-            initRect();
-            isFirstLaunch = false;
-        }
-        if (isFirstNeedRedraw) {
-            first = new TextDrawable("111", Color.RED, Color.BLUE, new RectF());
-            first.draw(canvas);
-            isFirstNeedRedraw = false;
-            canvas.save();
-        }
-        if (isSecondNeedRedraw) {
-            second = new TextDrawable("222", Color.BLACK, Color.YELLOW, boundsFirst);
-            second.draw(canvas);
+        initRect(canvas);
+
+    }
+
+    public void startAnim() {
+        invalidate();
+    }
+
+
+    private void initRect(Canvas canvas) {
+        mMaxInvalidateCount = (int) (animationTime / mFps);
+        mPath = new Path();
+        float cx = getWidth() / 2;
+        float cy = getHeight() / 2;
+        float step = (getWidth() / 1.5f) / mMaxInvalidateCount;
+        int alphaStep = (int) (255f / mMaxInvalidateCount);
+        mHighAlpha -= alphaStep;
+        mLowAlpha += alphaStep;
+        mChangedRadius += step;
+
+        clipPath(canvas);
+        if (isFirstLayerVisible) {
+            //first layer
+            drawLayer(canvas, cx, cy, mFirstBackColorColor, step * mMaxInvalidateCount, mTextSize, mFirstTextColor, mHighAlpha);
+
+            //second layer
+            drawLayer(canvas, cx, cy, mSecondBackColorColor, mChangedRadius, mTextSize, mSecondTextColor, mLowAlpha);
+
+        } else {
+            //second layer
+            drawLayer(canvas, cx, cy, mSecondBackColorColor, step * mMaxInvalidateCount, mTextSize, mSecondTextColor, mHighAlpha);
+
+            //first layer
+            drawLayer(canvas, cx, cy, mFirstBackColorColor, mChangedRadius, mTextSize, mFirstTextColor, mLowAlpha);
+
         }
 
 
-        postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                boundsFirst.set(boundsFirst.left - step, boundsFirst.top - step, boundsFirst.right - step, boundsFirst.bottom - step);
-                invalidate();
+        if (mCurrentInvalidateCount <= mMaxInvalidateCount) {
+            mCurrentInvalidateCount++;
+            invalidate();
 
-            }
-        }, 10);
+        } else {
+            isFirstLayerVisible = !isFirstLayerVisible;
+            mChangedRadius = 0;
+            mCurrentInvalidateCount = 0;
+            mHighAlpha = 255;
+            mLowAlpha = 0;
+        }
 
 
     }
 
 
-    private void initRect() {
-        boundsFirst = new RectF(0, 0, getWidth(), getHeight());
-        boundsSecond = new RectF(0, 0, getWidth(), getHeight());
+    private void drawLayer(Canvas canvas, float cx, float cy, int backgroundColor, float radius, int textSize, int textColor, int textAlpha) {
+        Path textPath = new Path();
+        Paint textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setColor(textColor);
+        textPaint.setAlpha(textAlpha);
+        textPaint.setTextSize(textSize);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPath.moveTo(0, cy);
+        textPath.lineTo(getWidth(), cy);
+
+        mPath.addCircle(cx, cy, radius, Path.Direction.CW);
+        Paint pathPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pathPaint.setStyle(Paint.Style.FILL);
+        pathPaint.setColor(backgroundColor);
+        mPath.addPath(textPath);
+        canvas.drawPath(mPath, pathPaint);
+        canvas.drawTextOnPath("11111", textPath, 0, 0, textPaint);
+
+        mPath.reset();
+    }
+
+
+    private void clipPath(Canvas canvas) {
+        Path path = new Path();
+        path.addRoundRect(new RectF(4, 4, getWidth() - 4, getHeight() - 4), convertDpToPixel(5), convertDpToPixel(5), Path.Direction.CW);
+        canvas.clipPath(path);
+
     }
 
 
@@ -77,54 +138,5 @@ public class RevealTextView extends AppCompatTextView {
         return dp * ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 
-    class TextDrawable extends Drawable {
-
-        private final String text;
-        private final Paint textPaint;
-        private final Paint backPaint;
-        private final RectF rectF;
-
-        public TextDrawable(String text, int textColor, int backColor, RectF rect) {
-
-            this.rectF = rect;
-            this.text = text;
-
-            textPaint = new Paint();
-            textPaint.setColor(textColor);
-            textPaint.setTextSize(14f);
-            textPaint.setAntiAlias(true);
-            textPaint.setStyle(Paint.Style.FILL);
-            textPaint.setTextAlign(Paint.Align.CENTER);
-
-            backPaint = new Paint();
-            backPaint.setColor(backColor);
-            backPaint.setAntiAlias(true);
-            backPaint.setStyle(Paint.Style.FILL);
-
-        }
-
-        @Override
-        public void draw(@NonNull Canvas canvas) {
-            canvas.drawRoundRect(rectF, 5, 5, backPaint);
-            canvas.drawText(text, getWidth() / 2, getHeight() / 2, textPaint);
-        }
-
-        @Override
-        public void setAlpha(int alpha) {
-            textPaint.setAlpha(alpha);
-            backPaint.setAlpha(alpha);
-        }
-
-        @Override
-        public void setColorFilter(ColorFilter cf) {
-            textPaint.setColorFilter(cf);
-            backPaint.setColorFilter(cf);
-        }
-
-        @Override
-        public int getOpacity() {
-            return PixelFormat.TRANSLUCENT;
-        }
-    }
 
 }
